fc4400865aeb31845161dc431721b16d28af4c8b
Celine Podgornik <31493506+capodgornik@users.noreply.github.com>
2022-07-05 11:08:41 -0700

    Create weight.cpp

---
+++ weight.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+  double weighte; //weight on earth
+  double weightm;  //weight on Mars
+
+  std::cout << "Enter the weight of an item on Earth in pounds: ";
+  std::cin >> weighte;
+  weightm = (weighte/9.81) * 3.711;
+  std::cout << "The weight of the item on Mars would be: " << weightm << "pounds.";
+
+}
