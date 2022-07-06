9b5be5b6f33c0a625a603471983217af58417046
Celine Podgornik <31493506+capodgornik@users.noreply.github.com>
2022-07-06 10:34:08 -0700

    Update weight.cpp

--- weight.cpp
+++ weight.cpp
@@ -8,6 +8,6 @@ int main() {
   std::cout << "Enter the weight of an item on Earth in pounds: ";
   std::cin >> weighte;
   weightm = (weighte/9.81) * 3.711;
-  std::cout << "The weight of the item on Mars would be: " << weightm << "pounds.";
+  std::cout << "The weight of the item on Mars would be: " << weightm << " pounds.";
 
 }
